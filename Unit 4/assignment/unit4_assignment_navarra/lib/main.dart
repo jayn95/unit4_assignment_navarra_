edd8db1e37b47b50a54e83feb18037a2a691289c
jayn95 <jill.navarra@wvsu.edu.ph>
2024-09-27 20:55:47 +0800

    added new files

--- Unit 4/assignment/unit4_assignment_navarra/lib/main.dart
+++ Unit 4/assignment/unit4_assignment_navarra/lib/main.dart
@@ -7,119 +7,222 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Profile App',
+      home: ProfileScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: Padding(
+          padding: EdgeInsets.only(left: 60.0),
+          child: Text(
+            'Jill Valente Navarra',
+            style: TextStyle(
+              fontSize: 19,
+              fontFamily: 'Vonique',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
+        backgroundColor: const Color.fromARGB(255, 221, 131, 153),
+        foregroundColor: Colors.white,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          // Profile Container with Background Image and adjusted size
+          Container(
+            height: 180, // Adjust height as needed
+            padding: const EdgeInsets.all(16.0),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/wall.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: const Align(
+              alignment: Alignment.centerLeft, // Aligns the content to the left
+              child: Row(
+                children: [
+                  SizedBox(width: 70),
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage(
+                        'assets/images/jay.png'), // Ensure this path is correct
+                  ),
+                  SizedBox(width: 16),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Jill Navarra',
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          'BS in Computer Science - WVSU',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Container for Information and Biography sections
+          Expanded(
+            child: Row(
+              children: [
+                // Information Section
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(60.0),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 241, 185,
+                          191), // Set a light blue background for the information section
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Information',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        const SizedBox(height: 10),
+                        Expanded(
+                          child: ListView(
+                            children: [
+                              _buildInfoRow(Icons.cake, 'Age           ', '21'),
+                              _buildInfoRow(Icons.email, 'Email        ',
+                                  'jill.navarra@wvsu.edu.ph'),
+                              _buildInfoRow(
+                                  Icons.phone, 'Phone      ', '+123 456 7890'),
+                              _buildInfoRow(Icons.location_city, 'Address   ',
+                                  'Pototan, Iloilo'),
+                              _buildInfoRow(Icons.school, 'Education',
+                                  'BS in Computer Science'),
+                              _buildInfoRow(
+                                  Icons.favorite, 'Fav Artist ', 'T.S.'),
+                              _buildInfoRow(Icons.brush, 'Hobbies   ',
+                                  'Art, Food Trip, Journal'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // Biography Section
+                Container(
+                  width: 1000, // Adjust width as needed
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 255, 252,
+                        243), // Background color for the biography section
+                  ),
+                  child: Center(
+                    // Center the entire inner container
+                    child: Container(
+                      width: 890,
+                      height: 450, // Set a new width
+                      padding: const EdgeInsets.all(
+                          50), // Padding for the inner container
+                      decoration: BoxDecoration(
+                        color: Colors
+                            .white, // Background color for the inner container
+                        borderRadius: BorderRadius.circular(
+                            3), // Optional: rounded corners
+                        border: Border.all(
+                          // Add border outline
+                          color: Colors.grey, // Border color
+                          width: 0.5, // Border width
+                        ),
+                      ),
+                      child: const Column(
+                        // Use Column to stack title and text
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'This is My Story',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Hello! I am Jill Navarra a third year student from BS in Computer Science. '
+                            'I have a passion for trying out new things and learning new skills. Fun fact about me is that'
+                            'I am an introvert witha black Golden Retriever personality. I like express myself through Art'
+                            'but at the same time I have a deep interest in Science and Mathematics. I aim for growth and improvement in my life.'
+                            ' Someday, I want to inspire others to dream big and never give up. Even if we failed so many times, just try again. '
+                            'Even if we lost something important in our life, something even better will come. Even if we made a mistake, there are'
+                            ' more chances for change. Even if life is imperfect, it is still beautiful. Even if we stumble again and again and again, we must go on.'
+                            'And I believe that the personal values we earned from the battles we fought and the hardships we endured are what makes us a better person.'
+                            ' And by becoming a better person, the world starts to become better also. And lastly, all this things are possible with trust and faith in God.'
+                            ' So this is me, the real me!',
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(IconData icon, String label, String data) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Row(
+            children: [
+              Icon(icon, size: 24),
+              const SizedBox(width: 8),
+              Text(
+                label,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          const SizedBox(width: 16), // Add space between label and data
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                data,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
